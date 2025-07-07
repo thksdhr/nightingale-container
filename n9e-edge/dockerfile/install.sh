a9f64c5abc801b5650595ca6e8d85be11b2eb360
taozig <thksdhr@icloud.com>
2025-07-07 11:01:46 +0800

    测试自动更新

--- n9e-edge/dockerfile/install.sh
+++ n9e-edge/dockerfile/install.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 SHELL_PATH=$(readlink -f "${BASH_SOURCE[0]}"); SHELL_DIR=$(dirname $SHELL_PATH); cd "$SHELL_DIR" || { echo "切换工作目录失败"; exit 1; }
+
+# download jq
+apt update && apt install -y jq
+
+# download file
+echo "downloadging..."
+N9E_TAG=$(curl -s https://api.github.com/repos/ccfos/nightingale/releases/latest | jq -r '.tag_name')
+echo "N9E_TAG: $N9E_TAG"
+wget -O ${SHELL_DIR}/temp/n9e.tar.gz https://github.com/ccfos/nightingale/releases/download/${N9E_TAG}/n9e-${N9E_TAG}-linux-amd64.tar.gz
+
+# switch work directory
+echo "switch work directory..."
+cd ${SHELL_DIR}/temp
+echo "current work directory: $(pwd)"
+
+# uzip
+tar -zxvf n9e.tar.gz
+
+# cp file
+echo "cp file..."
+cp -f ${SHELL_DIR}/temp/n9e-edge ${SHELL_DIR}/n9e-edge/n9e-edge
+cp -f ${SHELL_DIR}/temp/etc/edge/edge.toml ${SHELL_DIR}/n9e-edge/etc/edge.toml
+
+# done
+echo "done"
