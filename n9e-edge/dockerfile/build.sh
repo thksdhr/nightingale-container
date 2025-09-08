2d9ceb6e68c36a39aa439f637d8f1f853b72a986
taozig <thksdhr@icloud.com>
2025-09-08 15:33:42 +0800

    更新版本至v8.3.1

--- n9e-edge/dockerfile/build.sh
+++ n9e-edge/dockerfile/build.sh
@@ -3,7 +3,7 @@ SHELL_PATH=$(readlink -f "${BASH_SOURCE[0]}"); echo "SHELL_PATH=[$SHELL_PATH]"; 
 cd "$SHELL_DIR" || { echo "切换工作目录失败"; exit 1; }
 
 # ENV
-TAG="v8.2.2"
+TAG="v8.3.1"
 
 # build
 echo "开始构建n9e-edge镜像..."
